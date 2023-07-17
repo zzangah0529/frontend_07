2f06140dec4d557ee93ade8b1a04c4581c46f5b9
zzangah0529 <131572550+zzangah0529@users.noreply.github.com>
2023-07-17 16:10:28 +0900

    Delete routes directory

--- routes/root.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const { getIndex } = require('../controllers/root.js');
-
-// http://localhost:3000/
-router.get('/', getIndex)
-
-module.exports = router; 
